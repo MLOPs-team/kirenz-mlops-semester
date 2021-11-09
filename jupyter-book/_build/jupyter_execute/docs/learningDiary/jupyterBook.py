d1ed261e674ffc619133f1235417a3d5c3e6d037
Pascal Illg <pascal.illg@gmx.de>
2021-11-09 09:50:59 +0100

    added folder requiredTheoryKirenz to jupyter-book

---
+++ jupyter-book/_build/jupyter_execute/docs/learningDiary/jupyterBook.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Jupyter-Book good to know
+# 
+# 
+# ## General Information 
+# 
+# All files are in the folder docs.
+
+# ## Sections in a file
+
+# In[1]:
+
+
+# Chapter 1 title
+
+## Chapter 1 second header
+
+### Chapter 1 third header
+
+#### Chapter 1 section title
+
+##### Chapter 1 section second header
+
+
+# ### Table of Content
+# 
+# The title '# Chapter 1 Title' of the file is listed in the table of contents (see on the left)
+# 
+# 
+# The second, third.... header ist listed in the table of comtents on the top right hand corner
+# 
+# If you added a new file in the learning diary or technical documentation, you also need to update the _toc.yml.
+# You just need to add the added file in the expecting chapter.
+
+# #### How to build the Jupyter-Book
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+jupyter-book build jupyter-book/
+
---
+++ jupyter-book/_build/jupyter_execute/docs/learningDiary/requiredTheoryKirenz/questionsCollection.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notes: Questions for lecture on 17.11
+# 
